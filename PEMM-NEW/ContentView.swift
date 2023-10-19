e3da154d862d6bacd2528a3fd5a49d88e604d012
MariaBruno1 <125269434+MariaBruno1@users.noreply.github.com>
2023-10-19 11:56:56 +0200

    add

---
+++ PEMM-NEW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PEMM-NEW
+//
+//  Created by Maria Bruno on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PEMM-NEW/PEMM_NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  PEMM_NEWApp.swift
+//  PEMM-NEW
+//
+//  Created by Maria Bruno on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PEMM_NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
