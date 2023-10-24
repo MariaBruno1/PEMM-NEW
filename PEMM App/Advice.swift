68ced52bbeff8c3766f9f101e34704b4424899fe
MariaBruno1 <125269434+MariaBruno1@users.noreply.github.com>
2023-10-24 18:39:44 +0200

    red

---
+++ PEMM App/Advice.swift
@@ -0,0 +1,16 @@
+//
+//  Advices.swift
+//  PEMM App
+//
+//  Created by Maria Bruno on 24/10/23.
+//
+
+import Foundation
+struct Advice: Identifiable {
+    var id = UUID()
+    var name: String
+    var description: String
+    var imageName: String
+}
+
+
