a1f4602f1823a4591bc81c3fdbcd11e947ab7fce
FruggMatt <148440360+FruggMatt@users.noreply.github.com>
2023-10-24 10:31:57 +0200

    wew

--- PEMM App/ExerciseListView.swift
+++ PEMM App/ExerciseListView.swift
@@ -24,7 +24,7 @@ struct ExerciseListView: View {
                                 }
                             }
                         }
-                        .navigationTitle ("Select exercise")
+                        .navigationTitle ("Select the exercise")
                     }
                     
                 }
